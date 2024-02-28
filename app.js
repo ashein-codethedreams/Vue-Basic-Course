a5fa8bdbc9d673d274f4fac4cdc0839127b59079
Entt Nine <heroglasses155@gmail.com>
2024-02-28 21:27:26 +0630

    mouse and key events

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
 const app = Vue.createApp({
   data() {
     return {
-      title: "Student",
-      name: "Hla Hla",
-      age: 23,
+      infos: [
+        {
+          title: "Student",
+          name: "Hla Hla",
+          age: 23,
+        },
+        {
+          title: "Programmer",
+          name: "Ashein",
+          age: 25,
+        },
+      ],
       show: true, //value for show div
     };
+  },
+  methods: {
+    mouseoverHandler() {
+      console.log("mouseover is overworking");
+    },
+    mouseleaveHandler() {
+      console.log("mouse is leaving");
+    },
+    dblclickHandler() {
+      console.log("double ckicked");
+    },
   },
 });
 app.mount("#app");
